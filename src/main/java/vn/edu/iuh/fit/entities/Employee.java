f7ba4bed1b7ab693c57c2428ab5c44d34920c042
Ngoc Bich <bichdao090202@gmail.com>
2023-09-13 19:10:28 +0700

    update employee.java file

--- src/main/java/vn/edu/iuh/fit/entities/Employee.java
+++ src/main/java/vn/edu/iuh/fit/entities/Employee.java
@@ -1,4 +1,90 @@
 package vn.edu.iuh.fit.entities;
 
+import java.util.Date;
+
 public class Employee {
+    private long id;
+
+    private String fullname;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    private Date dob;
+
+    public Employee(long id) {
+        this.id = id;
+    }
+
+    public Employee(long id, String fullname, String email, String phone, String address, Date dob) {
+        this.id = id;
+        this.fullname = fullname;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.dob = dob;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullname='" + fullname + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", dob=" + dob +
+                '}';
+    }
 }
